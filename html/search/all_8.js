d47c60f7bd92377e3a5f1e8e7b9af50e2c3e2c8c
Doxygen CI <ci-arduino@invalid>
2025-11-26 16:22:34 +0000

    Deploy docs to GitHub Pages from commit ab57171ee2
    Commit: ab57171ee2c9302365d3573d0259a1f6bdee6cdd
    GitHub Actions run: 19710392215

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -3,6 +3,7 @@ var searchData=
   ['setchannel',['setChannel',['../class_adafruit___n_a_u7802.html#a9692eae9c2e2359d57fde96cc4d6b2a9',1,'Adafruit_NAU7802']]],
   ['setgain',['setGain',['../class_adafruit___n_a_u7802.html#a3016c18f9bcdd5a5f466eb97ec5ec572',1,'Adafruit_NAU7802']]],
   ['setldo',['setLDO',['../class_adafruit___n_a_u7802.html#ab4dcb1b211fe46a751a8fc923cb97b9f',1,'Adafruit_NAU7802']]],
+  ['setpgabypass',['setPGABypass',['../class_adafruit___n_a_u7802.html#ad3be93b85f0fca2fbbffd3faea5048fd',1,'Adafruit_NAU7802']]],
   ['setpgacap',['setPGACap',['../class_adafruit___n_a_u7802.html#a9933ee664ba16349e3d05bf584eb1960',1,'Adafruit_NAU7802']]],
   ['setrate',['setRate',['../class_adafruit___n_a_u7802.html#a495d8b6a65baa39488143d5f6403efc8',1,'Adafruit_NAU7802']]]
 ];
